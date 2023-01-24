2169413d69dcc4d3e1c2b654e9a04f534559b464
Desubicator <francopirinoli@gmail.com>
2023-01-24 16:22:11 -0300

    changed file structure, created components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
+
 
 const app = () => {
   return (
-    <div>app</div>
+    <BrowserRouter> 
+        <Box sx={{backgroundColor: "#000" }}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
 }
 
