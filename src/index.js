29500845ccfc0aa6406cffa1d57ff90db6621abb
Desubicator <francopirinoli@gmail.com>
2023-01-24 15:59:00 -0300

    Changed file structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
