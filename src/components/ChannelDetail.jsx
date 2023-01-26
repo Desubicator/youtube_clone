8c726d789e53577b255924d2bb4a475778991971
Desubicator <francopirinoli@gmail.com>
2023-01-25 22:11:56 -0300

    Implemented Channel Details

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,23 +1,34 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { Videos, ChannelCard } from "./";
+import { videos, ChannelCard } from "./";
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  const [ChannelDetail, setChannelDetail] = useState(null);
+  const [channelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([]);
 
   const { id } = useParams();
-  console.log(ChannelDetail)
 
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
-      fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
       .then((data) => setVideos(data?.items))
-      .then((data) => setChannelDetail(data?.items[0]));
   }, [id]);
+
+  return (
+
+    <Box minHeight="95vh" >
+      <Box>
+        <div style={{background: "linear-gradient(90deg, rgba(16,61,111,1) 17%, rgba(0,212,255,1) 100%)", zIndex: 10, height: "300px"}}/>
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+    </Box>
+  )
 }
+
+
 
 export default ChannelDetail
