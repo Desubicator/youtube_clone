404fa8df6e441b65d73b1188ebc3a91064a7fe74
Desubicator <francopirinoli@gmail.com>
2023-01-28 18:49:00 -0300

    fixed uri characters not being replaced in titles

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,7 +7,7 @@ const Videos = ({videos, direction}) => {
   if(!videos?.length) return "Loading...";
 
   return(
-    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="Start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
